6abaf7521f9c785357a1b4c71c793326fbb811c4
Richard Julian <rjulian57@gmail.com>
2017-12-08 07:27:13 -0800

    Add feature of taking filename as argument

--- yq.go
+++ yq.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-
+	"io/ioutil"
 	"gopkg.in/yaml.v2"
 )
 
@@ -14,9 +14,18 @@ func usage() {
 	println("Bad usage")
 }
 
+func parse_yaml(text string, queryString string) {
+	m := make(map[interface{}]interface{})
+	err := yaml.Unmarshal([]byte(text), &m)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("%v", m[queryString[1:]])
+}
+
 func main() {
 	flag.Parse()
-	if len(flag.Args()) != 1 {
+	if len(flag.Args()) < 1 || len(flag.Args()) > 2 {
 		usage()
 		os.Exit(-1)
 	}
@@ -25,19 +34,23 @@ func main() {
 		usage()
 		os.Exit(-1)
 	}
-	buf := make([]byte, 0, 4*1024)
-	reader := bufio.NewReader(os.Stdin)
-	s, err := reader.Read(buf[:cap(buf)])
-	if err != nil {
-		fmt.Printf("Error reading text: %v", err)
-		os.Exit(-1)
+	if len(flag.Args()) == 2 {
+		dat, err := ioutil.ReadFile(flag.Args()[1])
+		if err != nil {
+			fmt.Printf("Error reading text: %v", err)
+			os.Exit(-1)
+		}
+		text := string(dat)
+		parse_yaml(text, queryString)
+	} else {
+		buf := make([]byte, 0, 4*1024)
+		reader := bufio.NewReader(os.Stdin)
+		s, err := reader.Read(buf[:cap(buf)])
+		if err != nil {
+			fmt.Printf("Error reading text: %v", err)
+			os.Exit(-1)
+		}
+		text := string(buf[:s])
+		parse_yaml(text, queryString)
 	}
-	text := string(buf[:s])
-	m := make(map[interface{}]interface{})
-	err = yaml.Unmarshal([]byte(text), &m)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	fmt.Printf("%v", m[queryString[1:]])
 }
